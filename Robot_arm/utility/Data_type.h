9aa7e75082c8e782d8a2c4236a69facb0db4af32
Abhinay Kumar <kumarabhinay53@gmail.com>
2025-03-26 13:56:58 +0530

    Add files via upload

---
+++ Robot_arm/utility/Data_type.h
@@ -0,0 +1,25 @@
+#ifndef DATA_TYPE_H
+#define DATA_TYPE_H
+
+#include <stdint.h>
+#include <string>
+
+typedef uint8_t       Ec_uint_8;
+typedef int8_t        Ec_int_8;
+typedef uint16_t      Ec_uint_16;
+typedef int16_t       Ec_int_16;
+typedef uint32_t      Ec_u_int_32;
+typedef int32_t       Ec_int_32;
+typedef uint64_t      Ec_u_int_64;
+typedef int64_t       Ec_int_64;
+typedef float         Ec_float;
+typedef double        Ec_double;
+typedef long          Ec_long;
+typedef long long     Ec_long_long;
+typedef unsigned long Ec_u_long;
+typedef char          Ec_char;
+typedef std::string   Ec_string;
+typedef bool          Ec_boolean;
+
+
+#endif // DATA_TYPE_H
